b245a98e1a89696dcbe63113d4f54a548f2f7035
Marc Raaz <mnraaz@gmail.com>
2019-11-12 12:12:16 +1000

    Add files via upload

---
+++ app/Models/Archive_Attendee.js
@@ -0,0 +1,9 @@
+'use strict'
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Model = use('Model')
+
+class Archive_Attendee extends Model {
+}
+
+module.exports = Archive_Attendee
