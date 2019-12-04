3591494c42711ba2e442d9ff59fede0b18258cbf
Marc Raaz <mnraaz@gmail.com>
2019-12-05 09:46:10 +1000

    upgraded packages

--- resources/assets/js/mainApp/components/___components - Work.bat
+++ resources/assets/js/mainApp/components/___components - Work.bat
@@ -1,4 +1,4 @@
-pscp -i "C:\Users\marc.raaz\Google Drive\Projects\AWS\myGame.ppk" AddEsportsExp.js ubuntu@3.104.72.55:/projects/myGame/resources/assets/js/mainApp/components
+pscp -i "C:\Users\marc.raaz\Google Drive\Projects\AWS\myGame.ppk" Dossier.js ubuntu@3.104.72.55:/projects/myGame/resources/assets/js/mainApp/components
 ping 127.0.0.1 -n 4 > nul
 
 #AddEsportsExp.js
