fcb262433f1042fb362fe8a1c0d27f845926970e
myg <myg@Felipes-MacBook-Pro.local>
2020-01-26 19:38:13 -0300

    Improving Loading of Components

---
+++ resources/assets/js/mainApp/components/Messenger/MessengerLoader.js
@@ -0,0 +1,59 @@
+
+import React from "react";
+import { connect } from 'react-redux';
+
+import Messenger from './Messenger';
+import LoadingIndicator from '../LoadingIndicator';
+
+import { prepareMessengerAction } from '../../../redux/actions/chatAction';
+
+class MessengerLoader extends React.PureComponent {
+
+  state = {
+    loaded: false,
+  }
+
+  componentDidUpdate() {
+    if (!this.state.loaded && !this.props.loading) {
+      this.props.prepareMessenger(this.props.pin, !this.props.publicKey);
+      this.setState({ loaded: true });
+    }
+  }
+
+  render() {
+    if (!this.state.loaded || this.props.preparingMessenger) {
+      return (
+        <section id="messenger">
+          <div className="messenger-loading-container">
+            <p className="messenger-loading-hint-top">Hang On</p>
+            <div className="messenger-loading-indicator">
+              <LoadingIndicator />
+            </div>
+            <p className="messenger-loading-hint-bottom">Loading Messenger...</p>
+          </div>
+        </section>
+      );
+    }
+    return <Messenger
+      profileImage={this.props.profileImage}
+      userId={this.props.userId}
+      publicKey={this.props.publicKey}
+    />;
+  }
+
+}
+
+function mapStateToProps(state) {
+  return {
+    preparingMessenger: state.chat.preparingMessenger,
+    pin: state.encryption.pin,
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return ({
+    prepareMessenger: (pin, newKeys) => dispatch(prepareMessengerAction(pin, newKeys)),
+  });
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(MessengerLoader);
