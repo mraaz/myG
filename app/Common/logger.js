042d39c5b1c66e294430dccb4ad1b65314ae6e88
Felipe Roque <felipe-roque@Felipes-MacBook-Pro.local>
2020-03-29 22:04:13 -0300

    Color Coding Terminal (very important)

--- app/Common/logger.js
+++ app/Common/logger.js
@@ -3,7 +3,12 @@
 const ALLOWED_LOGS = ['USER', 'CHAT', 'GUEST', 'CRON'];
 
 function log(context, args) {
-  if (ALLOWED_LOGS.includes(context)) console.log(context, args);
+  if (ALLOWED_LOGS.includes(context)) {
+    if (context === 'USER') console.log('\x1b[34m', context, args, '\x1b[0m');
+    if (context === 'CHAT') console.log('\x1b[33m', context, args, '\x1b[0m');
+    if (context === 'GUEST') console.log('\x1b[36m', context, args, '\x1b[0m');
+    if (context === 'CRON') console.log('\x1b[35m', context, args, '\x1b[0m');
+  }
 }
 
 module.exports = { log };
