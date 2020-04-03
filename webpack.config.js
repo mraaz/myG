b89cff0589b6a66f7d069108b7901bc400b3bc59
nitin <nitintyagi@admins-MacBook-Air.local>
2020-04-03 14:27:24 +0530

    inti

--- webpack.config.js
+++ webpack.config.js
@@ -1,74 +1,114 @@
-
-const path = require('path');
+const path = require('path')
 const webpack = require('webpack')
-const VENDOR_LIBS = [
-  'redux', 'react-redux', 'react-dom'
-]
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const CompressionPlugin = require('compression-webpack-plugin')
+
+const mode = process.env.NODE_ENV == 'production' ? process.env.NODE_ENV : 'development'
+const VENDOR_LIBS = ['redux', 'react-redux', 'react-dom']
+const outputPath = path.resolve(__dirname, 'public/js/components')
 
 module.exports = {
   entry: {
     mainApp: './resources/assets/js/mainApp/index.js',
-    vendor: VENDOR_LIBS
+    vendor: VENDOR_LIBS,
   },
-  output: { filename: '[name].js',
-            path: path.resolve(__dirname, 'public/js/components') },
+  output: {
+    filename: '[name].js',
+    path: outputPath,
+    chunkFilename: '[id].[chunkhash].chunk.js',
+  },
+  devtool: mode == 'development' ? 'source-map' : '',
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        options: {
-          presets: [
-            [ 'es2015', { modules: false } ],
-            'stage-0', 'react'
-          ]
-        }
+        test: /\.(js|jsx)$/,
+        exclude: [/node_modules/],
+        use: 'babel-loader',
       },
       {
         test: /\.scss$/,
-        use: [{
-          loader: "style-loader"
-        }, {
-          loader: "css-loader", options: {
-            sourceMap: true
-          }
-        }, {
-          loader: "sass-loader", options: {
-            sourceMap: true
-          }
-        }]
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
       },
       {
         test: /\.css$/,
-        use: [{
-          loader: "style-loader"
-        }, {
-          loader: "css-loader", options: {
-            sourceMap: true
-          }
-        }, {
-          loader: "sass-loader", options: {
-            sourceMap: true
-          }
-        }],
-      }
-    ]
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+    ],
   },
   plugins: [
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      minChunks: function (module) {
-        // this assumes your vendor imports exist in the node_modules directory
-        return module.context && module.context.indexOf('node_modules') !== -1;
-      }
+    new webpack.HotModuleReplacementPlugin(),
+    new CompressionPlugin({
+      include: /\/includes/,
+      algorithm: 'gzip',
+      test: /\.js$|\.css$|\.html$|\.eot?.+$|\.ttf?.+$|\.woff?.+$|\.svg?.+$/,
+      threshold: 10240,
+      minRatio: 0.8,
+      compressionOptions: { level: 9 },
     }),
-    //new webpack.optimize.UglifyJsPlugin({
-    // sourceMap: options.devtool && (options.devtool.indexOf("sourcemap") >= 0 || options.devtool.indexOf("source-map") >= 0)
-    //}),
-    //new webpack.Define
-    //new webpack.optimize.CommonsChunkPlugin({
-    //  name: 'manifest' //But since there are no more common modules between them we end up with just the runtime code included in the manifest file
-    //})
-  ]
-};
+  ],
+  optimization: {
+    moduleIds: 'hashed',
+    runtimeChunk: 'single',
+    occurrenceOrder: true,
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /node_modules/,
+          chunks: 'all',
+          priority: 1,
+          name: 'vendor',
+        },
+      },
+    },
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        uglifyOptions: {
+          compress: true,
+          ecma: 6,
+          mangle: true,
+          warnings: false,
+          output: {
+            comments: false,
+          },
+        },
+        sourceMap: true,
+      }),
+    ],
+    removeAvailableModules: true,
+  },
+  mode,
+}
