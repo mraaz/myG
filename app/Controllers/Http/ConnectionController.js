421bf96851da69dc70543b7e5a8536b1b5c0e6d3
Marc Raaz <mnraaz@gmail.com>
2020-04-07 19:51:14 +1000

    part_2

--- app/Controllers/Http/ConnectionController.js
+++ app/Controllers/Http/ConnectionController.js
@@ -7,6 +7,34 @@ class ConnectionController {
   async gamers_you_might_know({ auth, request, response }) {
     if (auth.user) {
       try {
+        //pull data from table, limit to 8
+      } catch (error) {
+        console.log(error)
+      }
+    } else {
+      return 'You are not Logged In!'
+    }
+  }
+
+  async master_controller({ auth, request, response }) {
+    if (auth.user) {
+      try {
+        //run all checks and update the score
+
+      } catch (error) {
+        console.log(error)
+      }
+    } else {
+      return 'You are not Logged In!'
+    }
+  }
+
+  async check_if_same_profile({ auth, request, response }) {
+    //with the same gamess as your in your profile
+    if (auth.user) {
+      try {
+
+
       } catch (error) {
         console.log(error)
       }
