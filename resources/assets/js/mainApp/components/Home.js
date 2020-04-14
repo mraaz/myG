9004d039f99c695d964699d3242edb013b398371
nitin <nitintyagi@admins-MacBook-Air.local>
2020-04-14 12:48:12 +0530

    tab working

--- resources/assets/js/mainApp/components/Home.js
+++ resources/assets/js/mainApp/components/Home.js
@@ -10,6 +10,7 @@ export default class Home extends Component {
     this.state = {
       name: 'Raaz',
       initialData: undefined,
+      tabName: 'home',
     }
   }
   componentDidMount() {
@@ -18,30 +19,41 @@ export default class Home extends Component {
     })
   }
 
+  tabToggle = (tabName) => {
+    this.setState({
+      tabName: tabName,
+    })
+  }
+
   render() {
-    if (this.state.initialData != undefined) {
+    const { tabName, initialData } = this.state
+    if (initialData) {
       return (
         <div className='content-area'>
           <AnalyticsBox />
           <ComposeSection initialData={this.state.initialData == undefined ? 'loading' : this.state.initialData} />
           <div className='links'>
-            <Link to={`/`} className='link active'>
+            <div className={`${tabName == 'home' ? 'active' : ''}`} onClick={(e) => this.tabToggle('home')}>
               Home
-            </Link>
-            <Link to={`/groups`} className='link'>
+            </div>
+            <div className={`${tabName == 'communities' ? 'active' : ''}`} onClick={(e) => this.tabToggle('communities')}>
               Communities
-            </Link>
-            <Link to={`/notifications`} className='link'>
-              notifications
-            </Link>
-            <Link to={`/mygames`} className='link'>
+            </div>
+            <div className={`${tabName == 'notifications' ? 'active' : ''}`} onClick={(e) => this.tabToggle('notifications')}>
+              Notifications
+            </div>
+            <div className={`${tabName == 'mygames' ? 'active' : ''}`} onClick={(e) => this.tabToggle('mygames')}>
               My Games
-            </Link>
-            <Link to={`/mypost`} className='link'>
+            </div>
+            <div className={`${tabName == 'myposts' ? 'active' : ''}`} onClick={(e) => this.tabToggle('myposts')}>
               My Posts
-            </Link>
+            </div>
           </div>
-          <Posts initialData={this.state.initialData == undefined ? 'loading' : this.state.initialData} />
+          {tabName == 'home' && <Posts initialData={this.state.initialData == undefined ? 'loading' : this.state.initialData} />}
+          {tabName == 'communities' && <h1>communities</h1>}
+          {tabName == 'notifications' && <h1>notifications</h1>}
+          {tabName == 'mygames' && <h1>mygames</h1>}
+          {tabName == 'myposts' && <h1>myposts</h1>}
         </div>
       )
     } else {
