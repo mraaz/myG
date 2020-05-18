0a2becd9ddd3dd60e0d3bed0c3af73c9f5a416c7
Nitin tyagi <nitin.1992tyagi@gmail.com>
2020-05-18 15:37:46 +0530

    commnet post error

--- resources/assets/js/mainApp/components/MyPosts.js
+++ resources/assets/js/mainApp/components/MyPosts.js
@@ -117,21 +117,21 @@ export default class MyPosts extends Component {
           initialData={this.props.initialData == undefined ? 'loading' : this.props.initialData}
         />
         {isFetching && (
-          <div class='timeline-item'>
-            <div class='animated-background'>
-              <div class='background-masker header-top'></div>
-              <div class='background-masker header-left'></div>
-              <div class='background-masker header-right'></div>
-              <div class='background-masker header-bottom'></div>
-              <div class='background-masker subheader-left'></div>
-              <div class='background-masker subheader-right'></div>
-              <div class='background-masker subheader-bottom'></div>
-              <div class='background-masker content-top'></div>
-              <div class='background-masker content-first-end'></div>
-              <div class='background-masker content-second-line'></div>
-              <div class='background-masker content-second-end'></div>
-              <div class='background-masker content-third-line'></div>
-              <div class='background-masker content-third-end'></div>
+          <div className='timeline-item'>
+            <div className='animated-background'>
+              <div className='background-masker header-top'></div>
+              <div className='background-masker header-left'></div>
+              <div className='background-masker header-right'></div>
+              <div className='background-masker header-bottom'></div>
+              <div className='background-masker subheader-left'></div>
+              <div className='background-masker subheader-right'></div>
+              <div className='background-masker subheader-bottom'></div>
+              <div className='background-masker content-top'></div>
+              <div className='background-masker content-first-end'></div>
+              <div className='background-masker content-second-line'></div>
+              <div className='background-masker content-second-end'></div>
+              <div className='background-masker content-third-line'></div>
+              <div className='background-masker content-third-end'></div>
             </div>
           </div>
         )}
