82220d7281baa4cc716914052d003f846b87ea96
Marc Raaz <mnraaz@gmail.com>
2020-05-22 22:49:52 +1000

    stupid bug

--- app/Controllers/Http/PostHashTagTransactionController.js
+++ app/Controllers/Http/PostHashTagTransactionController.js
@@ -1,6 +1,24 @@
 'use strict'
+const Database = use('Database')
+const PostHashTagTransaction = use('App/Models/PostHashTagTransaction')
 
 class PostHashTagTransactionController {
+  async store({ auth }, post_id, hash_tag_id) {
+    if (auth.user) {
+      try {
+        const create_arrTags = PostHashTagTransaction.create({
+          post_id: post_id,
+          hash_tag_id: hash_tag_id,
+        })
+        return
+      } catch (error) {
+        if (error.code == 'ER_DUP_ENTRY') {
+          return
+        }
+        console.log(error)
+      }
+    }
+  }
 }
 
 module.exports = PostHashTagTransactionController
