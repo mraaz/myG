2f5e427d673867801c58ac2dd9adac86e15af2a9
Felipe Roque <felipe.roque@outlook.com>
2020-06-07 23:39:40 -0300

    Chat Passphrase Min Length

--- app/Controllers/Http/AuthController.js
+++ app/Controllers/Http/AuthController.js
@@ -37,7 +37,7 @@ class AuthController {
       email: 'required|email|unique:users',
       password: 'required|min:6|max:40',
       confirm_password: 'required',
-      encryption: 'required|min:12|max:30',
+      encryption: 'required|min:7|max:30',
       firstName: 'required',
       lastName: 'required',
     }
@@ -45,7 +45,7 @@ class AuthController {
     const messages = {
       required: 'Required field',
       email: 'Enter valid email address',
-      min: 'Not enough characters - Min 4 for Alias, 6 for Password and 12 for Encryption Paraphrase',
+      min: 'Not enough characters - Min 4 for Alias, 6 for Password and 7 for Encryption Paraphrase',
       max: 'Wow! Too many characters - Max 30',
       unique: 'Sorry, this field is not unique. Try again please.',
     }
