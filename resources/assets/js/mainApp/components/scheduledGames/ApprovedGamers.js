ec404bf6ecb63c46ce0037c1a3692a0acecde9dd
Nitin tyagi <nitin.1992tyagi@gmail.com>
2020-06-14 23:38:45 +0530

    gamer view done

--- resources/assets/js/mainApp/components/scheduledGames/ApprovedGamers.js
+++ resources/assets/js/mainApp/components/scheduledGames/ApprovedGamers.js
@@ -43,20 +43,56 @@ const Approved_gamers = (props) => {
             )
           })}
       </div>
-      {/* <div className='View__AllGamers' onClick={showModal}>
+      <div className='View__AllGamers' onClick={showModal}>
         View All Gamers
-      </div> */}
+      </div>
       <div className={`modal-container View__AllGamers__modal ${modalStatus ? 'modal--show' : ''}`}>
         <div className='modal-wrap'>
           <div className='modal__header'>
             Joined Gamers
-            <div className='modal__close' onClick={showModal}></div>
+            <div className='modal__close' onClick={showModal}>
+              <img src='https://mygame-media.s3-ap-southeast-2.amazonaws.com/platform_images/Dashboard/X_icon.svg' />
+            </div>
           </div>
           <div className='modal__body'>
-            {attendees.length > 0 &&
+            <div className='people_group_list_box_header'>
+              <div className='box_header_name' style={{ flex: 3 }}>
+                Gamer
+              </div>
+              <div className='box_header_name'>Server</div>
+              <div className='box_header_name'>Medals</div>
+              <div className='box_header_name'>Position</div>
+              <div className='box_header_name'>Level</div>
+            </div>
+
+            <div className='people_group_list_box'>
+              <div className='list__item'>
+                <div className='gamer__name'>
+                  <div className='default_circle'>{/* <img src={group_in.group_img} className='groupImage' /> */}</div>
+                  <spam>Nitin</spam>
+                </div>
+                <div className='other__title server'> Europe</div>
+                <div className='other__title medals'> Europe</div>
+                <div className='other__title'>
+                  {' '}
+                  <span className='position'>1</span>
+                </div>
+                <div className='other__title level'>
+                  {'level '}
+                  <span className='level__value'>99</span>
+                </div>
+              </div>
+              <div className='list__item'>
+                <div className='gamer__name'>
+                  <div className='default_circle'>{/* <img src={group_in.group_img} className='groupImage' /> */}</div>
+                  <spam>Nitin</spam>
+                </div>
+              </div>
+            </div>
+            {/* {attendees.length > 0 &&
               attendees.map((attendee) => {
                 return attendee.alias
-              })}
+              })} */}
           </div>
         </div>
 
