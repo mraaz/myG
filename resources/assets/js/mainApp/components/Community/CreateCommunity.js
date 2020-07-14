0e68dd03890a31af1adfaa4f4412a61854e2cf97
umair-alii <umairsajjadcs@gmail.com>
2020-07-14 17:24:28 +0500

    community file structure

---
+++ resources/assets/js/mainApp/components/Community/CreateCommunity.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react'
+
+const CreateCommunity = () => {
+  return (
+    <Fragment>
+      <div>
+        <h1>Create Community</h1>
+      </div>
+    </Fragment>
+  )
+}
+
+export default CreateCommunity
