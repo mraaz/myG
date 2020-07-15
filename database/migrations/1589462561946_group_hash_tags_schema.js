11d75f6b2885e7cf693f48c07a6682fe019abab2
Marc Raaz <mnraaz@gmail.com>
2020-07-15 22:28:40 +1000

    heartbeat

---
+++ database/migrations/1589462561946_group_hash_tags_schema.js
@@ -0,0 +1,24 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class GroupHashTagsSchema extends Schema {
+  up () {
+    this.create('group_hash_tags', (table) => {
+      table.increments()
+      table.string('content', 250).notNullable().unique().index()
+      table.integer('user_id').unsigned().notNullable()
+      table.foreign('user_id').references('users.id').onDelete('cascade')
+      table.integer('verified').unsigned().notNullable().defaultTo(0)
+      table.integer('counter').unsigned().nullable().defaultTo(0)
+      table.timestamps(true,true)
+    })
+  }
+
+  down () {
+    this.drop('group_hash_tags')
+  }
+}
+
+module.exports = GroupHashTagsSchema
