da1ffa078c415d80e2ed6501cc2142c5ea2e925c
umair-alii <umairsajjadcs@gmail.com>
2020-07-16 18:02:12 +0500

    create community screen UI hash_tags and feature image

--- resources/assets/js/mainApp/components/Community/CreateCommunity.js
+++ resources/assets/js/mainApp/components/Community/CreateCommunity.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react'
 import '../../styles/Community/community.scss'
+const buckectBaseUrl = 'https://mygame-media.s3-ap-southeast-2.amazonaws.com/platform_images/'
 import {
   SETTINGS_ENUMS,
   styles,
@@ -27,22 +28,34 @@ const CreateCommunity = () => {
             <MyGInput className='community-input' placeholder='Community Name' />
             <div className='fieldTitle'>Game Title</div>
             <MyGInput className='community-input' placeholder='Enter Game Title' />
+            {/* <div className='fieldTitle'>Featured Image</div>
+            <MyGTextarea
+              onChange={(event) => {
+                updateAdvancedSettings({ acceptMessage: event.target.value })
+              }}
+              placeholder='Create a message for those who join & accept your game'
+              maxLength={250}
+            /> */}
             <div className='fieldTitle'>Featured Image</div>
-            <MyGTextarea
-              onChange={(event) => {
-                updateAdvancedSettings({ acceptMessage: event.target.value })
-              }}
-              placeholder='Create a message for those who join & accept your game'
-              maxLength={250}
-            />
-            <div className='fieldTitle'>Add Hashtags</div>
-            <MyGTextarea
-              onChange={(event) => {
-                updateAdvancedSettings({ acceptMessage: event.target.value })
-              }}
-              placeholder='Create a message for those who join & accept your game'
-              maxLength={250}
-            />
+            <div className='community-media'>
+              <div className='media__container'>
+                <div className='fieldTitle'>Drop your image or video</div>
+                <div className='community-image'>
+                  <span className=' button photo-btn'>
+                    <img src={`${buckectBaseUrl}Dashboard/BTN_Attach_Image.svg`} />
+                  </span>
+                </div>
+                <div className='fieldTitle'>
+                  Or <span className='click-here-span'>click here </span> to select
+                </div>
+              </div>
+            </div>
+            <div className='hashTag_section'>
+              <div className='fieldTitle'>Add Hashtags</div>
+              <div className='hashtag_input'>
+                <MyGCreateableSelect isClearable isMulti className='hash_tag_name_box' placeholder='Search, Select or create Hash Tags' />
+              </div>
+            </div>
           </div>
         </div>
         <div className='right-container'>
