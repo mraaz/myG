7da4ab6f34c330c2dc27c0b294bb1897aeb423ee
Marc Raaz <mnraaz@gmail.com>
2020-08-11 23:07:12 +1000

    missed one

--- app/Controllers/Http/Schedule_games_logix.js
+++ app/Controllers/Http/Schedule_games_logix.js
@@ -16,7 +16,62 @@ class Schedule_games_logix {
           region: false,
         }
         break
-      default:
+      case 'League of Legends':
+        header_struct = {
+          experience: true,
+          platform: false,
+          region: false,
+        }
+        break
+      case 'Mobile Legends: Bang Bang':
+        header_struct = {
+          experience: true,
+          platform: false,
+          region: true,
+        }
+        break
+      case 'Overwatch':
+        header_struct = {
+          experience: true,
+          platform: true,
+          region: false,
+        }
+        break
+      case 'Call of Duty: Warzone':
+        header_struct = {
+          experience: true,
+          platform: true,
+          region: false,
+        }
+        break
+      case 'CSGO':
+        header_struct = {
+          experience: true,
+          platform: true,
+          region: false,
+        }
+        break
+      case 'Fortnite':
+        header_struct = {
+          experience: true,
+          platform: true,
+          region: false,
+        }
+        break
+      case 'PUBG':
+        header_struct = {
+          experience: true,
+          platform: false,
+          region: false,
+        }
+        break
+      case 'Rocket League':
+        header_struct = {
+          experience: true,
+          platform: true,
+          region: false,
+        }
+        break
     }
     return header_struct
   }
