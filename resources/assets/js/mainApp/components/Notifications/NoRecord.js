3cf0c772e0faf87e187978043dc0b49fa048e7d2
Nitin tyagi <nitin.1992tyagi@gmail.com>
2020-08-23 14:58:21 +0530

    bug fix and alert api inytegrated done

---
+++ resources/assets/js/mainApp/components/Notifications/NoRecord.js
@@ -0,0 +1,27 @@
+/*
+ * Author : nitin Tyagi
+ * github  : https://github.com/realinit
+ * Email : nitin.1992tyagi@gmail.com
+ */
+import React, { Fragment, useEffect, useState } from 'react'
+import axios from 'axios'
+import { Link } from 'react-router-dom'
+
+const NoRecordFound = (props) => {
+  return (
+    <div className='noRecordFound__wraper'>
+      <h1 className='noRecordFound__header'>Cannot find any games</h1>
+      <div className='noRecordFound__body'>
+        Try searching for another title or{' '}
+        <Link to='/addScheduleGames'>
+          {' '}
+          <span className='addScheduleGames__link' style={{ color: '#e5c746', cursor: 'pointer' }}>
+            add your own game
+          </span>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default NoRecordFound
