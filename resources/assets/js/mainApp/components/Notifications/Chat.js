a094a090bdb4c2d8b86d1c30d81aabbf7b205ecc
Nitin tyagi <nitin.1992tyagi@gmail.com>
2020-08-30 00:30:43 +0530

    chat api added but not working

--- resources/assets/js/mainApp/components/Notifications/Chat.js
+++ resources/assets/js/mainApp/components/Notifications/Chat.js
@@ -7,22 +7,29 @@ export default class Chat extends Component {
     super()
   }
 
-  componentDidMount = () => {
+  componentDidMount = async () => {
     window.scrollTo(0, 0)
+    this.setState({ fetching: true })
+    const chatNotifications = await axios.post(`/api/chat_notifications?page=1`)
+    console.log('chatNotifications  ', chatNotifications)
+
+    if (chatNotifications.data.length > 0) {
+      this.setState({ chatNotifications: chatNotifications.data, fetching: false }, () => {
+        this.props.setNotificationsCount(this.state.chatNotifications.length)
+      })
+    }
   }
 
   render() {
-    const { active } = this.props;
+    const { active } = this.props
 
-    const isActive = active == true ? {display: 'block'} : {display: 'none'};
+    const isActive = active == true ? { display: 'block' } : { display: 'none' }
 
     return (
       <div style={isActive}>
         <TopTabs tabs={['All', 'Messages', 'Groups', 'Misc']} />
-        <div className='endline'>
-          You don't have new notifications
-        </div>
+        <div className='endline'>You don't have new notifications</div>
       </div>
-    );
+    )
   }
 }
