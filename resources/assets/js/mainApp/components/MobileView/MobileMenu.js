2ac1ef0376ccdd045885bc91194bc1f9f26a8fe7
Bilal Bhatti <bilal@yobo.dev>
2020-09-04 17:01:51 +0500

    added hide and show

--- resources/assets/js/mainApp/components/MobileView/MobileMenu.js
+++ resources/assets/js/mainApp/components/MobileView/MobileMenu.js
@@ -1,17 +1,18 @@
-import React, { Component, Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 
-class MobileMenu extends Component {
-  render() {
-    return (
-      <Fragment>
-        <section className='main-mobile-menu'>
-          <div className='menu-bottom'>
-            <div className='mobile-sub-menu'>
-              <div className='mobile-feed-img'>
-                <img src='https://mygame-media.s3.amazonaws.com/platform_images/Dashboard/btn_Feed.svg' class='img-fluid' />
-              </div>
+const MobileMenu = () => {
+  const [hideSubMenu, setHideSubMenu] = useState(false)
+  return (
+    <Fragment>
+      <section className='main-mobile-menu'>
+        <div className='menu-bottom'>
+          <div className='mobile-sub-menu'>
+            <div className='mobile-feed-img'>
+              <img src='https://mygame-media.s3.amazonaws.com/platform_images/Dashboard/btn_Feed.svg' class='img-fluid' />
             </div>
-            <div className='mobile-search-img'>
+          </div>
+          <div className='mobile-search'>
+            {hideSubMenu && (
               <div className='mobile-sub-menu-items'>
                 <div className='find-matches'>
                   <a href='#'>
@@ -24,28 +25,30 @@ class MobileMenu extends Component {
                   </a>
                 </div>
               </div>
+            )}
+            <div className='mobile-search-img' onClick={() => setHideSubMenu(!hideSubMenu)}>
               <img src='https://mygame-media.s3.amazonaws.com/platform_images/Dashboard/btn_Search.svg' class='img-fluid' />
             </div>
-            <div className='mobile-sub-menu'>
-              <div className='mobile-create-img'>
-                <img src='https://mygame-media.s3.amazonaws.com/platform_images/Dashboard/btn_New_Game.svg' class='img-fluid' />
-              </div>
-            </div>
-            <div className='mobile-sub-menu'>
-              <div className='mobile-profile-img'>
-                <img src='https://mygame-media.s3.amazonaws.com/platform_images/Dashboard/btn_Profile.svg' class='img-fluid' />
-              </div>
-            </div>
-            <div className='mobile-sub-menu'>
-              <div className='mobile-notification-img'>
-                <img src='https://mygame-media.s3.amazonaws.com/platform_images/Dashboard/Bell_Icon.svg' class='img-fluid' />
-              </div>
+          </div>
+          <div className='mobile-sub-menu'>
+            <div className='mobile-create-img'>
+              <img src='https://mygame-media.s3.amazonaws.com/platform_images/Dashboard/btn_New_Game.svg' class='img-fluid' />
             </div>
           </div>
-        </section>
-      </Fragment>
-    )
-  }
+          <div className='mobile-sub-menu'>
+            <div className='mobile-profile-img'>
+              <img src='https://mygame-media.s3.amazonaws.com/platform_images/Dashboard/btn_Profile.svg' class='img-fluid' />
+            </div>
+          </div>
+          <div className='mobile-sub-menu'>
+            <div className='mobile-notification-img'>
+              <img src='https://mygame-media.s3.amazonaws.com/platform_images/Dashboard/Bell_Icon.svg' class='img-fluid' />
+            </div>
+          </div>
+        </div>
+      </section>
+    </Fragment>
+  )
 }
 
 export default MobileMenu
