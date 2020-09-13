7aec2b4436538ec71152f85116df0d5d4c140915
Nitin tyagi <nitin.1992tyagi@gmail.com>
2020-09-13 14:06:10 +0530

    community view route added

---
+++ resources/assets/js/mainApp/components/CommunityView/CommunityView.js
@@ -0,0 +1,33 @@
+import React, { useState, useRef, useEffect } from 'react'
+import { toast } from 'react-toastify'
+import moment from 'moment'
+import axios from 'axios'
+import { Toast_style, Convert_to_comma_delimited_value } from '../Utility_Function'
+
+import CoverImage from './CoverImage'
+
+const CommunityView = (props) => {
+  const [communityDetails, setCommunityDetails] = useState({})
+
+  useEffect(() => {
+    const getcommunityDetails = async () => {
+      const {
+        data: { getOne = {} },
+      } = await axios.get(`/api/groups/getGroupDetails/Hunter19oq235z`)
+
+      setCommunityDetails({ ...getOne })
+    }
+    getcommunityDetails()
+    return () => {
+      setCommunityDetails({})
+    }
+  }, [])
+
+  return (
+    <div className='communityName__container '>
+      <CoverImage {...communityDetails} />
+    </div>
+  )
+}
+
+export default CommunityView
---
+++ resources/assets/js/mainApp/components/CommunityView/CoverImage.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import { Toast_style, Convert_to_comma_delimited_value } from '../Utility_Function'
+
+const addDefaultSrc = (ev) => {
+  ev.target.src = 'https://mygame-media.s3.amazonaws.com/default_user/universe.jpg'
+}
+
+import AnalyticsBox from '../AnalyticsBox'
+
+const CoverImage = (props) => {
+  //   const [communityDetails, setCommunityDetails] = useState({})
+
+  return (
+    <div className='coverImage__container'>
+      <img onError={addDefaultSrc} src={props.group_img} />
+      {/* <img onError={addDefaultSrc} src={`https://9cover.com/images/ccovers/1365478680game-of-thrones.jpg`} /> */}
+
+      <div className='analyticsBox__container'>
+        <AnalyticsBox />
+      </div>
+      <div className='community__option'>
+        <div className='name'>{props.name}</div>
+        <div className='option'>
+          <button type='button' className='btnWarning'>
+            Join
+            <img src='https://mygame-media.s3.amazonaws.com/platform_images/View+Game/Down+Carrot_black.svg'></img>
+          </button>
+          <button type='button' className='btnWarning'>
+            Manage
+          </button>
+          <button type='button' className='btnWarning'>
+            View Members
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default CoverImage
