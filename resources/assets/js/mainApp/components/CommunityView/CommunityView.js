9a8132dbe8fe5e552b48af5d02e33967e7ba16da
Nitin tyagi <nitin.1992tyagi@gmail.com>
2020-09-15 16:31:08 +0530

    some more work

--- resources/assets/js/mainApp/components/CommunityView/CommunityView.js
+++ resources/assets/js/mainApp/components/CommunityView/CommunityView.js
@@ -27,6 +27,13 @@ const CommunityView = (props) => {
   return (
     <div className='communityName__container '>
       <CoverImage {...communityDetails} />
+      <div className='community__description'>
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+        erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus
+        est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
+        labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd
+        gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet
+      </div>
       {communityDetails.id && (
         <GamePosts {...props} group_id={communityDetails.id} current_user_permission={communityDetails.current_user_permission} />
       )}
