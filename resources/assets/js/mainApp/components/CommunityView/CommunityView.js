27f950bb9c05996582698e0c1654d5578bb14a51
Nitin tyagi <nitin.1992tyagi@gmail.com>
2020-10-02 01:12:54 +0530

    sponcers modal added

--- resources/assets/js/mainApp/components/CommunityView/CommunityView.js
+++ resources/assets/js/mainApp/components/CommunityView/CommunityView.js
@@ -1,15 +1,19 @@
 import React, { useState, useRef, useEffect } from 'react'
 import moment from 'moment'
 import axios from 'axios'
-import GamePosts from './GamePosts'
+import { Link } from 'react-router-dom'
 
+import GamePosts from './GamePosts'
 import CoverImage from './CoverImage'
 import Members from './Members'
+import MangeSponcers from './MangeSponcers'
 
 const CommunityView = (props) => {
   const [communityDetails, setCommunityDetails] = useState({})
   const [modalStatus, setModalStatus] = useState(false)
   const [activeModalTab, setActiveModalTab] = useState('settings')
+  const [showSponcerModal, setShowSponcerModal] = useState(false)
+  const [singleSponcer, setSingleSponcer] = useState({})
 
   useEffect(() => {
     const getcommunityDetails = async () => {
@@ -32,6 +36,39 @@ const CommunityView = (props) => {
     setModalStatus(!modalStatus)
   }
 
+  const handleSponcerClick = (data) => {
+    setSingleSponcer(data)
+    setShowSponcerModal(true)
+  }
+  const hideSponcerModal = (data) => {
+    setSingleSponcer({})
+    setShowSponcerModal(false)
+  }
+
+  const renderSponcers = (sponcers = []) => {
+    return (
+      <div className='sponcers__container'>
+        <div className='sponcers sponsors__label'>
+          <span>Sponsors</span>
+        </div>
+        {sponcers.length > 0 &&
+          sponcers.map((sponcer) => {
+            return (
+              <div className='sponcers' key={sponcer.id}>
+                <a href={`//${sponcer.link}`} target='_blank'>
+                  <img className='sponcers__image' src={`${sponcer.media_url}`} />
+                </a>
+                <div className='sponcers__edit' onClick={(e) => handleSponcerClick(sponcer)}>
+                  Edit
+                </div>
+                <div className='sponcers__delete'>Delete</div>
+              </div>
+            )
+          })}
+      </div>
+    )
+  }
+
   return (
     <div className='communityName__container '>
       <CoverImage {...communityDetails} handleModalStatus={handleModalStatus} {...props} />
@@ -39,6 +76,8 @@ const CommunityView = (props) => {
       <div className='community__tags'>
         <div className='label'>Tags :</div>
       </div>
+      {renderSponcers(communityDetails.sponsors)}
+      {showSponcerModal && <MangeSponcers sponsor={singleSponcer} handleModalStatus={hideSponcerModal} group_id={communityDetails.id} />}
       {communityDetails.id && (
         <GamePosts {...props} group_id={communityDetails.id} current_user_permission={communityDetails.current_user_permission} />
       )}
---
+++ resources/assets/js/mainApp/components/CommunityView/MangeSponcers.js
@@ -0,0 +1,84 @@
+import React from 'react'
+import axios from 'axios'
+import Modal from 'react-modal'
+import { Link } from 'react-router-dom'
+import SweetAlert from '../common/MyGSweetAlert'
+import { toast } from 'react-toastify'
+import { Toast_style } from '../Utility_Function'
+import { PageHeader, MyGButton, MyGModal, MyGInput } from '../common'
+
+export default class MangeSponcers extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      modalStatus: true,
+      saveButtonDisabled: true,
+      linkValue: '',
+      media_url: '',
+    }
+  }
+
+  addDefaultSrc = (ev) => {
+    ev.target.src = 'https://mygame-media.s3.amazonaws.com/default_user/universe.jpg'
+  }
+
+  updateSponcer = async (e) => {
+    const { sponsor = {}, groups_id } = this.props
+    const { linkValue, media_url } = this.state
+    const updateSponcer = await axios.post('/api/sponsor/update', {
+      group_id: groups_id,
+      id: sponsor.id,
+      media_url: media_url == '' ? sponsor.media_url : media_url,
+      link: linkValue == '' ? sponsor.link : linkValue,
+    })
+    if (updateSponcer) {
+      toast.error(<Toast_style text={'Great, Saved successfully!'} />)
+      this.props.handleModalStatus()
+    }
+  }
+
+  handleLinkChange = (e) => {
+    const data = e.target.value
+    this.setState({ linkValue: data, saveButtonDisabled: false })
+  }
+
+  render() {
+    const { sponsor = {} } = this.props
+    const { saveButtonDisabled = [], linkValue = '', media_url = '', modalStatus = true } = this.state
+
+    return (
+      <div className={`modal-container View__Member__modal ${modalStatus ? 'modal--show' : ''}`}>
+        <div className='modal-wrap'>
+          <div className='modal__header'>
+            <div className='tabs___header'>
+              <span className={`setting__tab `}>Edit Media</span>
+            </div>
+            <div className='modal__close' onClick={(e) => this.props.handleModalStatus()}>
+              <img src='https://mygame-media.s3.amazonaws.com/platform_images/Dashboard/X_icon.svg' />
+            </div>
+          </div>
+          <div className='modal__body sponcer__edit'>
+            <div className='sponcer__media__input'>
+              <img src={media_url == '' ? sponsor.media_url : media_url} onError={this.addDefaultSrc} />
+            </div>
+            <div className='sponcer__link__input'>
+              <input type='text' onChange={this.handleLinkChange} value={linkValue == '' ? sponsor.link : linkValue} />
+            </div>
+          </div>
+          <div className='modal__footer'>
+            <MyGButton
+              customStyles={{ color: '#FFFFFF', border: '2px solid #FFFFFF', background: '#000' }}
+              onClick={() => this.props.handleModalStatus()}
+              text='Cancel'
+            />
+            <button type='button' disabled={saveButtonDisabled} onClick={() => this.updateSponcer(true)}>
+              Save
+            </button>
+          </div>
+        </div>
+
+        <div className='modal-overlay' onClick={(e) => this.props.handleModalStatus()}></div>
+      </div>
+    )
+  }
+}
