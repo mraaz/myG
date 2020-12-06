d518610477a40cc4ab8a88651d9306cb3e379752
Marc Raaz <mnraaz@gmail.com>
2020-12-06 10:25:41 +1000

    Revert "css clean up"

---
+++ resources/assets/sass/pages/___pages.bat
@@ -0,0 +1,5 @@
+pscp -i "C:\Users\Raaz\Google Drive\Projects\AWS\myGame.ppk" _chat.scss ubuntu@3.104.72.55:/projects/myG/resources/assets/sass/pages
+ping 127.0.0.1 -n 5 > nul
+
+#_addscheduleGames
+#_scheduleGames.scss 
