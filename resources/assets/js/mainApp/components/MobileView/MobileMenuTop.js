cb11978c1c44527a615a11a58d8f261df03f33b3
Marc Raaz <mnraaz@gmail.com>
2020-12-09 23:25:59 +1000

    mobile screen fixes

--- resources/assets/js/mainApp/components/MobileView/MobileMenuTop.js
+++ resources/assets/js/mainApp/components/MobileView/MobileMenuTop.js
@@ -15,7 +15,7 @@ const MobileMenuTop = (props) => {
 
   return (
     <Fragment>
-      <div class={hide ? 'menu-tab hide' : 'menu-tab show' } onClick={() => setHideSideMenu(true)}>
+      <div class={hide ? 'menu-tab hide' : 'menu-tab show'} onClick={() => setHideSideMenu(true)}>
         <img src='https://mygame-media.s3.amazonaws.com/platform_images/Dashboard/logo.svg' class='img-fluid logo-img' />
         <div class='toggle-menu-btn'>
           <img src='https://mygame-media.s3.amazonaws.com/platform_images/Dashboard/toggle_menu_collapsed.svg' class='img-fluid' />
@@ -23,31 +23,19 @@ const MobileMenuTop = (props) => {
         <div className={'notification-expanded'}>
           <Link to='/?at=notifications&submenu=1'>
             <div className='notification-container'>
-              <img
-                src='https://mygame-media.s3.amazonaws.com/platform_images/Dashboard/ntfo_Friendship_Icon.svg'
-                height='22'
-                width='22'
-              />
+              <img src='https://mygame-media.s3.amazonaws.com/platform_images/Dashboard/ntfo_Friendship_Icon.svg' height='22' width='22' />
               <div className='notification-box'>{notifications.approvals}</div>
             </div>
           </Link>
           <Link to='/?at=notifications&submenu=2'>
             <div className='notification-container'>
-              <img
-                src='https://mygame-media.s3.amazonaws.com/platform_images/Dashboard/Bell_Icon.svg'
-                height='22'
-                width='22'
-              />
+              <img src='https://mygame-media.s3.amazonaws.com/platform_images/Dashboard/Bell_Icon.svg' height='22' width='22' />
               <div className='notification-box'>{notifications.alerts}</div>
             </div>
           </Link>
           <Link to='/?at=notifications&submenu=3'>
             <div className='notification-container'>
-              <img
-                src='https://mygame-media.s3.amazonaws.com/platform_images/Dashboard/Chat_Icon.svg'
-                height='22'
-                width='22'
-              />
+              <img src='https://mygame-media.s3.amazonaws.com/platform_images/Dashboard/Chat_Icon.svg' height='22' width='22' />
               <div className='notification-box'>{notifications.chats}</div>
             </div>
           </Link>
@@ -67,16 +55,18 @@ const MobileMenuTop = (props) => {
                 <div className='user-name-icon'>
                   <Link to={`/profile/${alias}`}>
                     <div className='user-icon'>
-                      <img onError={(ev) => {ev.target.src = 'https://mygame-media.s3.amazonaws.com/default_user/new-user-profile-picture.png'}}
-                           src={profileImage}
-                           className='img-fluid'
-                           alt='user-picture' />
+                      <img
+                        onError={(ev) => {
+                          ev.target.src = 'https://mygame-media.s3.amazonaws.com/default_user/new-user-profile-picture.png'
+                        }}
+                        src={profileImage}
+                        className='img-fluid'
+                        alt='user-picture'
+                      />
                     </div>
-                    <div className='user-name'>
-                      @{alias}
-                    </div>
+                    <div className='user-name'>@{alias}</div>
                   </Link>
-                </div>       
+                </div>
                 <div className='level-information'>
                   <div className='level-wrapper'>
                     <span className='level'>level</span>
@@ -84,35 +74,40 @@ const MobileMenuTop = (props) => {
                     <span className='level-numer'>{level}</span>
                   </div>
                 </div>
-                <div className='reputation-reviews'>
-                  <p className='reputation'>Reputation</p>
-                  <p className='reputation-number'>4.66/5</p>
-                  <p className='reputation-reviews-number'>556 reviews</p>
-                </div>
               </div>
             </div>
             <div className='menu-list'>
               <ul>
                 <li>
-                  <Link to='/' onClick={() => setHideSideMenu(false)}>Home</Link>
+                  <Link to='/' onClick={() => setHideSideMenu(false)}>
+                    Home
+                  </Link>
                 </li>
                 <li>
-                  <Link to='/?at=communities' onClick={() => setHideSideMenu(false)}>Communities</Link>
+                  <Link to='/?at=communities' onClick={() => setHideSideMenu(false)}>
+                    Communities
+                  </Link>
                 </li>
                 <li>
-                  <Link to='/?at=notifications&submenu=0' onClick={() => setHideSideMenu(false)}>Notifications</Link>
+                  <Link to='/?at=notifications&submenu=0' onClick={() => setHideSideMenu(false)}>
+                    Notifications
+                  </Link>
                 </li>
                 <li>
-                  <Link to='/?at=mygames' onClick={() => setHideSideMenu(false)}>My Games</Link>
+                  <Link to='/?at=mygames' onClick={() => setHideSideMenu(false)}>
+                    My Games
+                  </Link>
                 </li>
               </ul>
             </div>
             <div className='logout-setting-section'>
-              <div className='logout-btn' onClick={() => {
-                setHideSideMenu(false)
-                // dispatch(logoutAction())
-                window.location.href = '/logout'
-              }}>
+              <div
+                className='logout-btn'
+                onClick={() => {
+                  setHideSideMenu(false)
+                  // dispatch(logoutAction())
+                  window.location.href = '/logout'
+                }}>
                 <img src='https://mygame-media.s3.amazonaws.com/platform_images/Dashboard/Logout_Icon.svg' />
                 <span>Logout</span>
               </div>
