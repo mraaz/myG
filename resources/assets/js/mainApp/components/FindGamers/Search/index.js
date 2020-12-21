a2eef74cf33935b81077dc7c1f07eb4c86908d55
Felipe Roque <felipe.roque@outlook.com>
2020-12-21 06:08:40 -0300

    Clear Search + Info Button

--- resources/assets/js/mainApp/components/FindGamers/Search/index.js
+++ resources/assets/js/mainApp/components/FindGamers/Search/index.js
@@ -17,25 +17,32 @@ export default class Search extends React.Component {
     this.props.onSearch('', this.state.online);
   }
 
+  showHelp = () => showMessengerAlert(`
+    You can use this to search for gamers, try typing a country or a game you're interested in.
+    
+    You can also target the search for a specific area you want, which will restrict your results to it,
+    For example, if you want either Australians or Dota player, you can search for "Australia Dota",
+    But if you want only Australians that play Dota, you can search for "country: Australia game: Dota".
+
+    These are the areas that can be targeted:
+    alias: ?, country: ?, relationship: ?, commendations: ?, team: ?, languages: ?, game: ?
+  `);
+
   onChange = ({ target: { value: search } }) => {
     this.setState({ search });
-    if (search === ':?') showMessengerAlert(`
-      You can use this to search for gamers, try typing a country or a game you're interested in.
-      
-      You can also target the search for a specific area you want, which will restrict your results to it,
-      For example, if you want either Australians or Dota player, you can search for "Australia Dota",
-      But if you want only Australians that play Dota, you can search for "country: Australia game: Dota".
-
-      These are the areas that can be targeted:
-      alias: ?, country: ?, relationship: ?, commendations: ?, team: ?, languages: ?, game: ?
-    `)
+    if (search === ':?') this.showHelp();
     if (search.trim().length >= 3) this.props.onSearch(search.trim(), this.state.online);
+    if (search.trim().length < 3) this.props.onSearch('', this.state.online);
   }
 
   toggleOnline = () => {
     this.setState((previous) => ({ online: !previous.online }), () => {
       this.props.onSearch(this.state.search.trim(), this.state.online);
     });
+  }
+
+  helpButton = () => {
+    return <span className="help clickable" onClick={this.showHelp}>!</span>
   }
 
   render() {
@@ -49,6 +56,7 @@ export default class Search extends React.Component {
               value={this.state.search} 
               onChange={this.onChange}
             ></input>
+            {this.helpButton()}
           </div>
         </div>
         <div id='find-gamers-online' className='row'>
