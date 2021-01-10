e7c00c503c096331ddceb18034cd44fa56fa2e9e
Marc Raaz <mnraaz@gmail.com>
2021-01-10 11:02:25 +1000

    push

--- database/migrations/9580672082472_import_community_data.js
+++ database/migrations/9580672082472_import_community_data.js
@@ -6,7 +6,7 @@ const Schema = use('Schema')
 class Import_dummy_data_Schema extends Schema {
   up () {
     this
-    .raw("insert into groups (name, user_id, group_img, game_names_id, grp_description, created_at, updated_at) values ('StarCraft II', 1, 'grp_img', 998, 'grp_description',  '1988-08-08', '1988-08-08')")
+    .raw("insert into groups (name, user_id, group_img, game_names_id, grp_description, created_at, updated_at) values ('Dota 2 - myG Official', 1, 'https://static-cdn.jtvnw.net/ttv-boxart/Dota%202-600x320.jpg', 998, 'Offical community, hosted by myG',  '1988-08-08', '1988-08-08')")
 
 
 
