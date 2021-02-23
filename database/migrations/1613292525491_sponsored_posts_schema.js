3b7d9b548afb8bee98b3c8caa787b0373c04e80a
Marc Raaz <mnraaz@gmail.com>
2021-02-24 00:06:49 +1000

    sponsored posts fixed

--- database/migrations/1613292525491_sponsored_posts_schema.js
+++ database/migrations/1613292525491_sponsored_posts_schema.js
@@ -13,9 +13,9 @@ class SponsoredPostsSchema extends Schema {
       table.string('click_url', 2048).nullable()
       table.string('url', 2048).nullable()
       table.text('caption').collate('utf8mb4_unicode_ci')
-      table.integer('visibility').defaultTo(0)
-      table.integer('category').defaultTo(1)
-      table.integer('times_clicked').defaultTo(0)
+      table.integer('visibility').defaultTo(0).index()
+      table.integer('category').defaultTo(1).index()
+      table.integer('times_clicked').defaultTo(0).index()
       table.timestamps(true,true)
     })
   }
