285c80623bc63913cf03010b851da647d5465684
Nitin tyagi <nitin.1992tyagi@gmail.com>
2021-02-25 00:01:17 +0530

    last feb bug fix

--- resources/assets/js/mainApp/components/scheduledGames/ScheduleGames.js
+++ resources/assets/js/mainApp/components/scheduledGames/ScheduleGames.js
@@ -228,9 +228,10 @@ export default class ScheduleGames extends Component {
 
         <section className='viewGame__container__mobile mobileView'>
           <MobileScheduledGames
-            scheduleGames={scheduleGamesView}
+            scheduleGames={scheduleGames}
             showFullGames={show_full_games}
             selectedGame={selected_game}
+            singleScheduleGamesPayload={singleScheduleGamesPayload}
             handleExcludesFullGames={this.handleExcludesFullGames}
             slideOptionLabel={this.state.slideOptionText}
             copyClipboardEnable={true}
