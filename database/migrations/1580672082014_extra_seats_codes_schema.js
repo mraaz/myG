5fae7468dc3134ef1675139a627ab13a589abe20
Marc Raaz <mnraaz@gmail.com>
2021-03-12 10:39:21 +1000

    minor

--- database/migrations/1580672082014_extra_seats_codes_schema.js
+++ database/migrations/1580672082014_extra_seats_codes_schema.js
@@ -7,7 +7,7 @@ class ExtraSeatsCodesSchema extends Schema {
   up () {
     this.create('extra_seats_codes', (table) => {
       table.increments()
-      table.string('code', 88)
+      table.string('code', 15)
       table.string('content', 254)
       table.timestamp('expiry').notNullable().defaultTo("2028-08-08 08:00:00")
       table.integer('counter').unsigned().defaultTo(0)
