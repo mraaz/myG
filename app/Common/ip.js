cd97abfb1fbd6630c3f001ff4609737290a52ae3
Marc Raaz <mnraaz@gmail.com>
2021-03-25 16:42:25 +1000

    tryu

--- app/Common/ip.js
+++ app/Common/ip.js
@@ -1,6 +1,8 @@
 function getIp(request) {
   const { headers, connection, socket } = request
   const connectionSocket = connection && connection.socket
+  console.log(headers, '<<<headers')
+  console.log(request, '<<<request')
   return (
     (headers && headers['x-forwarded-for']) ||
     (connection && connection.remoteAddress) ||
