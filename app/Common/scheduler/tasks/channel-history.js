df08bfb9dfeafc41e984c7a2852d553b557c7fdf
Marc Raaz <mnraaz@gmail.com>
2021-04-05 23:54:37 +1000

    fixing Redis

--- app/Common/scheduler/tasks/channel-history.js
+++ app/Common/scheduler/tasks/channel-history.js
@@ -3,7 +3,9 @@ module.exports = async (job) => {
   if (process.env.SCHEDULER_LOGS)
     console.log('\x1b[36m', 'SCHEDULER', moment.format('D MMM HH:mm:ss'), '-', `Started execution for ${job.name}`, '\x1b[0m')
   const guardExecution = require('./execution-guard')
-  //await guardExecution(job.name);
+  await guardExecution(job.name)
+  console.log('RUNNING THIS JOB')
+  console.log(job.name)
   const ChatRepository = require('../../../Repositories/Chat')
   await ChatRepository.clearChannelHistory(job)
   if (process.env.SCHEDULER_LOGS)
