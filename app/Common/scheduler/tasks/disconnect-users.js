30beb019af8bdee7e07b7661473bb335a9791ad7
Marc Raaz <mnraaz@gmail.com>
2021-04-06 12:21:37 +1000

    fixing some Redis lock issues

--- app/Common/scheduler/tasks/disconnect-users.js
+++ app/Common/scheduler/tasks/disconnect-users.js
@@ -1,7 +1,13 @@
 module.exports = async (job) => {
-  const moment = require('moment')();
-  if (process.env.SCHEDULER_LOGS) console.log('\x1b[36m', 'SCHEDULER', moment.format('D MMM HH:mm:ss'), '-', `Started execution for ${job.name}`, '\x1b[0m');;
+  const moment = require('moment')()
+  if (process.env.SCHEDULER_LOGS)
+    console.log('\x1b[36m', 'SCHEDULER', moment.format('D MMM HH:mm:ss'), '-', `Started execution for ${job.name}`, '\x1b[0m')
+
+  const guardExecution = require('./execution-guard')
+  await guardExecution(job.name)
+
   const ConnectionRepository = require('../../../Repositories/Connection')
   await ConnectionRepository.disconnectUsers()
-  if (process.env.SCHEDULER_LOGS) console.log('\x1b[36m', 'SCHEDULER', moment.format('D MMM HH:mm:ss'), '-', `Finished execution for ${job.name}`, '\x1b[0m');;
+  if (process.env.SCHEDULER_LOGS)
+    console.log('\x1b[36m', 'SCHEDULER', moment.format('D MMM HH:mm:ss'), '-', `Finished execution for ${job.name}`, '\x1b[0m')
 }
--- app/Common/scheduler/tasks/send-daily-emails.js
+++ app/Common/scheduler/tasks/send-daily-emails.js
@@ -1,9 +1,11 @@
 module.exports = async (job) => {
-  const moment = require('moment')();
-  if (process.env.SCHEDULER_LOGS) console.log('\x1b[36m', 'SCHEDULER', moment.format('D MMM HH:mm:ss'), '-', `Started execution for ${job.name}`, '\x1b[0m');;
+  const moment = require('moment')()
+  if (process.env.SCHEDULER_LOGS)
+    console.log('\x1b[36m', 'SCHEDULER', moment.format('D MMM HH:mm:ss'), '-', `Started execution for ${job.name}`, '\x1b[0m')
   const guardExecution = require('./execution-guard')
-  await guardExecution(job.name);
+  await guardExecution(job.name)
   const EmailController = require('../../../Controllers/Http/EmailController')
   await new EmailController().dailyEmails(job)
-  if (process.env.SCHEDULER_LOGS) console.log('\x1b[36m', 'SCHEDULER', moment.format('D MMM HH:mm:ss'), '-', `Finished execution for ${job.name}`, '\x1b[0m');
+  if (process.env.SCHEDULER_LOGS)
+    console.log('\x1b[36m', 'SCHEDULER', moment.format('D MMM HH:mm:ss'), '-', `Finished execution for ${job.name}`, '\x1b[0m')
 }
