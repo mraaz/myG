8b9ad58dfa063f18f34a8a783ae1a6f557d5a742
Felipe Roque <felipe.roque@outlook.com>
2021-04-24 05:31:25 -0300

    Allowing External Trigger

--- app/Controllers/Http/SchedulerController.js
+++ app/Controllers/Http/SchedulerController.js
@@ -4,13 +4,21 @@ const Database = use('Database')
 const { getJobs } = require('../../Common/scheduler')
 
 class SchedulerController {
-  async triggerJob({ auth, params, response }) {
+  async triggerJob({ auth, params, request, response }) {
     try {
-      const requestingUserId = auth.user.id
-      if (!requestingUserId) throw new Error('No User Logged In')
-      const securityCheck = await Database.from('admins').where({ user_id: requestingUserId, permission_level: 1 }).first()
-      const isAdmin = securityCheck && securityCheck.user_id == requestingUserId
-      if (!isAdmin) throw new Error('Feature only for admins')
+
+      if (auth && auth.user && auth.user.id) {
+        const requestingUserId = auth.user.id
+        const securityCheck = await Database.from('admins').where({ user_id: requestingUserId, permission_level: 1 }).first()
+        const isAdmin = securityCheck && securityCheck.user_id == requestingUserId
+        if (!isAdmin) return response.send("Invalid User Permissions", 403);
+      } else if (request.headers().authorization) {
+        const EXTERNAL_SCHEDULE_API_KEY = process.env.EXTERNAL_SCHEDULE_API_KEY || 'debug';
+        if (request.headers().authorization !== EXTERNAL_SCHEDULE_API_KEY) return response.send("Invalid Authentication Key", 403);
+      } else {
+        return response.send("Missing Authentication", 403);
+      }
+
       const jobs = getJobs()
       const validJobs = jobs.map((job) => job.name)
       const requestedJob = params.job
