f33946e1dc597ca221c88340e0689aeefc468752
Nitin tyagi <nitin.1992tyagi@gmail.com>
2021-05-29 23:46:57 +0530

    bug fix

--- resources/assets/js/mainApp/components/Profile/Profile/index.js
+++ resources/assets/js/mainApp/components/Profile/Profile/index.js
@@ -102,13 +102,13 @@ export class Profile extends React.Component {
           <ProfileInfo alias={this.props.alias} profile={this.props.profile} updateProfile={this.props.updateProfile} />
         </div>
          {!!sponsors.length && <Sponsors isSelf={this.props.profile.isSelf} alias={this.props.alias} profile={this.props.profile} sponsors={sponsors} refetchSponsors={() => this.props.fetchProfile(this.props.alias)} />}
-         <div className="desktopShow"> 
+         <div className="desktopShow zIndex"> 
           <GameExperiences userId={this.props.userId} selectedGame={this.props.gameId} commendUser={this.commendUser} deleteExperience={this.deleteExperience} alias={this.props.alias} profile={this.props.profile} updateGame={this.props.updateGame} />
         </div>
         <div className="mobileShow"> 
           <MobileGameExperiences userId={this.props.userId} selectedGame={this.props.gameId} commendUser={this.commendUser} deleteExperience={this.deleteExperience} alias={this.props.alias} profile={this.props.profile} updateGame={this.props.updateGame} />
         </div>
-        <div className="desktopShow"> 
+        <div className="desktopShow zIndex"> 
         {!!this.props.profile.isSelf  && <GamerSuggestions profile={this.props.profile} sendFriendRequest={this.props.sendFriendRequest} cancelFriendRequest={this.props.cancelFriendRequest} follow={this.props.follow} unfollow={this.props.unfollow}  /> }
         </div>
         <div className="mobileShow"> 
