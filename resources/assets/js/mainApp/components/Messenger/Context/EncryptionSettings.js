2b136d2acba871547d0580e19c4bf82339371cf7
Felipe Roque <felipe.roque@outlook.com>
2021-06-20 08:34:26 -0300

    Chat Password Type

--- resources/assets/js/mainApp/components/Messenger/Context/EncryptionSettings.js
+++ resources/assets/js/mainApp/components/Messenger/Context/EncryptionSettings.js
@@ -157,7 +157,7 @@ export default class EncryptionSettings extends React.Component {
     return (
       <input
         className='messenger-settings-encryption-key'
-        type='password'
+        type='text'
         placeholder='New Encryption Key'
         autoFocus
         disabled={!this.state.editingPin}
