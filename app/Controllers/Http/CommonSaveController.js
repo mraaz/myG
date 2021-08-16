1f9c753699be2f929dfc91321e21a4b0712c0236
Marc Raaz <mnraaz@gmail.com>
2021-08-17 09:17:08 +1000

    found more places

--- app/Controllers/Http/CommonSaveController.js
+++ app/Controllers/Http/CommonSaveController.js
@@ -21,7 +21,7 @@ class CommonSaveController {
   async register({ view, session }) {
     return view.render('auth/socialRegister', {
       alias: session.get('alias'),
-      email: session.get('email'),
+      email: session.get('email')
     })
   }
 
@@ -40,7 +40,7 @@ class CommonSaveController {
       email: 'required|email|unique:users',
       encryption: 'required|min:7|max:30',
       firstName: 'required',
-      lastName: 'required',
+      lastName: 'required'
     }
 
     const messages = {
@@ -48,7 +48,7 @@ class CommonSaveController {
       email: 'Enter valid email address',
       min: 'Not enough characters - Minimum 4 for Alias & 7 for Chat Password',
       max: 'Wow! Too many characters - Max 30',
-      unique: 'Sorry, this field is not unique. Try again please.',
+      unique: 'Sorry, this field is not unique. Try again please.'
     }
 
     const validation = await validate(request.all(), rules, messages)
@@ -98,7 +98,7 @@ class CommonSaveController {
           type: 'error',
           source: 'backend',
           context: __filename,
-          message: (error && error.message) || error,
+          message: (error && error.message) || error
         })
         session.withErrors(validation.messages()).flashAll()
         return response.redirect('back')
@@ -114,12 +114,12 @@ class CommonSaveController {
         return response.redirect('/?error=google-recaptcha')
       } else {
         // Seats Availability
-        const seatsAvailable = await SeatsAvailable.query().first()
-        const extraSeatsCode = request.input('extraSeatsCode')
-        const unlockedByCheatCode = request.input('unlockedByCheatCode')
-        if (!seatsAvailable.seats_available && !extraSeatsCode && !unlockedByCheatCode) {
-          return response.redirect('/?error=seats')
-        }
+        // const seatsAvailable = await SeatsAvailable.query().first()
+        // const extraSeatsCode = request.input('extraSeatsCode')
+        // const unlockedByCheatCode = request.input('unlockedByCheatCode')
+        // if (!seatsAvailable.seats_available && !extraSeatsCode && !unlockedByCheatCode) {
+        //   return response.redirect('/?error=seats')
+        // }
 
         const user = new User()
         user.first_name = await EncryptionRepository.encryptField(request.input('firstName'))
@@ -136,30 +136,30 @@ class CommonSaveController {
         await ChatRepository.publishOnMainChannel(`Introducing a newcomer to myG... Please all welcome ${user.alias} !!`)
 
         // Decrease Seats Available upon Registration
-        if (seatsAvailable.seats_available > 0) {
-          seatsAvailable.seats_available = (seatsAvailable.seats_available || 1) - 1
-          seatsAvailable.save()
-        }
+        // if (seatsAvailable.seats_available > 0) {
+        //   seatsAvailable.seats_available = (seatsAvailable.seats_available || 1) - 1
+        //   seatsAvailable.save()
+        // }
 
         // Mark Extra Seat Code as Used
-        if (extraSeatsCode) {
-          const extraSeatsCodes = await ExtraSeatsCodes.query()
-            .where('code', extraSeatsCode)
-            .first()
+        // if (extraSeatsCode) {
+        //   const extraSeatsCodes = await ExtraSeatsCodes.query()
+        //     .where('code', extraSeatsCode)
+        //     .first()
 
-          if (extraSeatsCodes != undefined) {
-            await ExtraSeatsCodes.query()
-              .where('code', extraSeatsCode)
-              .increment('counter', 1)
+        //   if (extraSeatsCodes != undefined) {
+        //     await ExtraSeatsCodes.query()
+        //       .where('code', extraSeatsCode)
+        //       .increment('counter', 1)
 
-            if (extraSeatsCodes.id) {
-              await ExtraSeatsCodesTran.create({
-                extra_seats_codes_id: extraSeatsCodes.id,
-                user_id: user.id,
-              })
-            }
-          }
-        }
+        //     if (extraSeatsCodes.id) {
+        //       await ExtraSeatsCodesTran.create({
+        //         extra_seats_codes_id: extraSeatsCodes.id,
+        //         user_id: user.id,
+        //       })
+        //     }
+        //   }
+        // }
 
         const connections = new ConnectionController()
         connections.master_controller({ auth })
