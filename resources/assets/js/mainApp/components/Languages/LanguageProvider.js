07e13b0cd29df056476b2431e8b560eb02a32919
NITIN TYAGI <nitintyagi@NITINs-MBP.mshome.net>
2021-08-29 01:11:04 +0530

    fix

--- resources/assets/js/mainApp/components/Languages/LanguageProvider.js
+++ resources/assets/js/mainApp/components/Languages/LanguageProvider.js
@@ -4,15 +4,18 @@ import { IntlProvider } from 'react-intl';
 
 import enTranslationMessages from './translations/en.json';
 import esTranslationMessages from './translations/es.json';
+import hiTranslationMessages from './translations/hi.json';
 
 export const languages = [
   { value: "en", label: { id: "settings.languages.english", defaultMessage: "English" } },
   { value: "es", label: { id: "settings.languages.spanish", defaultMessage: "Spanish" } },
+  { value: "hi", label: { id: "settings.languages.hindi", defaultMessage: "हिंदी" } },
 ];
 
 export const translationMessages = {
   en: enTranslationMessages,
   es: esTranslationMessages,
+  hi: hiTranslationMessages,
 };
 
 const LanguageProvider = ({ selectedLanguage, children }) => {
