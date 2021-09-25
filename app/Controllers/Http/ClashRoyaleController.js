eeb5265ce0d64b5d5bf9bccdb36dd23638fbcfeb
Marc Raaz <mnraaz@gmail.com>
2021-09-25 10:09:11 +1000

    part two

--- app/Controllers/Http/ClashRoyaleController.js
+++ app/Controllers/Http/ClashRoyaleController.js
@@ -13,20 +13,48 @@ class ClashRoyaleController {
       const token =
         'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjNkNmI1NzI5LWFmNjctNDgzNy1hMDY4LTUwYTQ5YWMwOWVmMyIsImlhdCI6MTYzMjQ2OTIwNCwic3ViIjoiZGV2ZWxvcGVyL2U0ZjA1ZjI4LWJmOGMtNDJmNS0yY2I1LTU0ZTZlNjA2N2QxMiIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyIxMjAuMjMuMTA2LjE1NyJdLCJ0eXBlIjoiY2xpZW50In1dfQ.A3ovMAL32_TLiHVqZPR9lsl40MKlisBcY13I7PLjD8uNEQibDdpNqffNbx06O7RFG2U59b_AyrurxgukZYZKOA'
 
-      // country = await axios.get(`https://api.clashroyale.com/v1/`)
-
       const config = {
         headers: { Authorization: `Bearer ${token}` }
       }
 
-      const clanTag = 'QG8UQCV0'
-      const showClanRequestURL = 'clans/' + '%23' + clanTag + '/members'
-      const showRiverRaceLogRequestURL = 'clans/' + '%23' + clanTag + '/riverracelog'
-      const showcurrentriverraceRequestURL = 'clans/' + '%23' + clanTag + '/currentriverrace'
+      const clanTag = 'YL9YCYU' //'QG8UQCV0'
+      const getClanURL = 'clans/' + '%23' + clanTag + '/members'
+      const getRiverRaceLogURL = 'clans/' + '%23' + clanTag + '/riverracelog'
+      const getCurrentriverraceURL = 'clans/' + '%23' + clanTag + '/currentriverrace'
 
-      const tmpValue = await axios.get(`https://api.clashroyale.com/v1/${showcurrentriverraceRequestURL}`, config)
-      return tmpValue.data
-      //return tmpValue
+      const getClanInfo = await axios.get(`https://api.clashroyale.com/v1/${getClanURL}`, config)
+      const getCurrentriverraceInfo = await axios.get(`https://api.clashroyale.com/v1/${getCurrentriverraceURL}`, config)
+
+      const isWarToday = getCurrentriverraceInfo.data.periodType == 'warDay' ? true : false
+      let riverRaceStruct = {}
+
+      if (isWarToday) {
+        for (let index = 0; index < getCurrentriverraceInfo.data.clan.participants.length; index++) {
+          let playerRiverDetails = {
+            decksUsed: getCurrentriverraceInfo.data.clan.participants[index].decksUsed,
+            decksUsedToday: getCurrentriverraceInfo.data.clan.participants[index].decksUsedToday,
+            fame: getCurrentriverraceInfo.data.clan.participants[index].fame,
+            repairPoints: getCurrentriverraceInfo.data.clan.participants[index].repairPoints,
+            boatAttacks: getCurrentriverraceInfo.data.clan.participants[index].boatAttacks
+          }
+
+          riverRaceStruct[getCurrentriverraceInfo.data.clan.participants[index].tag] = playerRiverDetails
+        }
+      }
+      for (let index = 0; index < getClanInfo.data.items.length; index++) {
+        if (isWarToday) {
+          getClanInfo.data.items[index].decksUsed = riverRaceStruct[getClanInfo.data.items[index].tag].decksUsed
+          getClanInfo.data.items[index].decksUsedToday = riverRaceStruct[getClanInfo.data.items[index].tag].decksUsedToday
+          getClanInfo.data.items[index].fame = riverRaceStruct[getClanInfo.data.items[index].tag].fame
+          getClanInfo.data.items[index].repairPoints = riverRaceStruct[getClanInfo.data.items[index].tag].repairPoints
+          getClanInfo.data.items[index].boatAttacks = riverRaceStruct[getClanInfo.data.items[index].tag].boatAttacks
+        } else {
+          getClanInfo.data.items[index].decksUsed = 0
+          getClanInfo.data.items[index].decksUsedToday = 0
+        }
+      }
+
+      return getClanInfo.data
     } catch (error) {
       LoggingRepository.log({
         environment: process.env.NODE_ENV,
