3e774f7c9d4dd2fb42b06ef006d7bf224bb168a9
David Lawson <david.lawson.95@outlook.com>
2022-03-04 17:16:10 +1000

    cleaning

--- microservices/services/clash-royale-proxy/src/common/middlewares.ts
+++ microservices/services/clash-royale-proxy/src/common/middlewares.ts
@@ -1,5 +1,4 @@
 import { Request, Response, NextFunction } from 'express';
-// import jwt from 'jsonwebtoken';
 
 /**
  * A custom authentication middleware which checks that the bearer token is valid
@@ -15,21 +14,4 @@ export const authenticateToken = (req: Request, res: Response, next: NextFunctio
   }
 
   next();
-
-  // Tokens expiration is ignored. Audience must match. Signature must match secret.
-  // jwt.verify(
-  //   token,
-  //   process.env.SECRET as string,
-  //   {
-  //     ignoreExpiration: true,
-  //     audience: 'myG_webserver',
-  //   },
-  //   (err: any, user: any) => {
-  //     if (err) {
-  //       console.error('error: ', err);
-  //       return res.sendStatus(403);
-  //     }
-  //     next();
-  //   }
-  // );
 };
